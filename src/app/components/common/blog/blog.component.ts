abbffd1c489515d0e7455f7556870d9a0a8f9d41
Murat Kuyubaşı <96579859+muratkuyubasi@users.noreply.github.com>
2023-11-27 10:03:00 +0300

    Haberr-Dinamik

--- src/app/components/common/blog/blog.component.ts
+++ src/app/components/common/blog/blog.component.ts
@@ -1,19 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
+import { ContentsService } from '../../services/contents.service';
+import { ContentsModel } from '../../models/contentsModel';
+import { environment } from 'src/environments/environment';
 
 @Component({
-    selector: 'app-blog',
-    templateUrl: './blog.component.html',
-    styleUrls: ['./blog.component.scss']
+	selector: 'app-blog',
+	templateUrl: './blog.component.html',
+	styleUrls: ['./blog.component.scss']
 })
 export class BlogComponent implements OnInit {
+	path = environment.serverUrl;
 
-    constructor() { }
+	newsModel: ContentsModel
 
-    ngOnInit(): void {
-    }
+	constructor(private contentsService: ContentsService) { }
 
-    blogSlides: OwlOptions = {
+	ngOnInit(): void {
+		this.getnewsList();
+	}
+
+	getnewsList() {
+		this.contentsService.getnewsList().subscribe(newsdata => {
+			this.newsModel = newsdata;
+		})
+	}
+
+	blogSlides: OwlOptions = {
 		loop: true,
 		nav: true,
 		dots: false,
@@ -35,6 +48,6 @@ export class BlogComponent implements OnInit {
 				items: 3
 			}
 		}
-    }
+	}
 
 }
